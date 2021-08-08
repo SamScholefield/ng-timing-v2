a232d70cc279b3130c48c5f8f3d9b942e79f4b6a
Sam Scholefield <sam.scholefield@openforce.com>
2021-08-08 16:56:22 +0200

    adds main component and basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppRouting } from './app-routing';
+import { HomeComponent } from './screens/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: AppRouting.PATH.HOME,
+    pathMatch: 'full',
+    data: {
+      pageTitle: undefined,
+    },
+  },
+  {
+    path: AppRouting.PATH.HOME,
+    component: HomeComponent,
+    // canActivate: [AuthGuard, DataAndPermissionGuard],
+    data: {
+      pageTitle: 'dashboard.title',
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/app-routing.ts
@@ -0,0 +1,29 @@
+export class AppRouting {
+  public static PATH = {
+    // AUTH
+    LOGIN: 'login',
+    REGISTER: 'register',
+    RESET: 'reset',
+
+    // CORE
+    HOME: 'home',
+    SETTINGS: 'settings',
+    PROFILE: 'profile',
+
+    // RIDERS
+    RIDER: 'rider',
+    RIDER_ADD: 'add-rider',
+    RIDER_EDIT: 'edit-rider',
+    RIDERS: 'riders',
+
+    // RACES
+    RACE: 'race',
+    RACE_ADD: 'add-race',
+    RACE_EDIT: 'edit-race',
+    RACES: 'races',
+
+    // RACES
+    RESULT: 'race-result',
+    RESULTS: 'results',
+  };
+}
